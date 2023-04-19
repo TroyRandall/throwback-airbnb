e8561c702d00ca56ad5ba45be7f1f455aaf2e0d8
TroyRandall <troyrandall18@gmail.com>
2023-04-19 19:33:22 -0400

    added sign up user already exists custom validators

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,49 +2,37 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const { Op } = require('sequelize');
 const { check, cookie } = require("express-validator");
-const { handleValidationErrors, UserAlreadyExistsErrors } = require("../../utils/validation");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
 
-validateUniqueName = [
-  check("username")
-    .custom( async (value , req, res, next) => {
-      const user = User.findOne({
-        where: {
-          'username': value
-        }
-      })
 
-      if(user){
-        const err = new Error('User already exists');
-        err.status = 403;
-        err.errors = [
-         "User with that username already exists"
-          ]
-
-          next(err);
-       }
-       next();
-    }),
-  UserAlreadyExistsErrors
-]
 const validateSignup = [
   check("email")
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage("Please provide a valid email."),
+    .withMessage("Invalid email"),
   check("username")
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
     .withMessage("Please provide a username with at least 4 characters."),
   check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("username")
+    .not().isEmpty()
+    .withMessage("Username is required"),
   check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
     .withMessage("Password must be 6 characters or more."),
+  check("firstName")
+    .not().isEmpty()
+    .withMessage("First name is required"),
+  check("lastName")
+    .not().isEmpty()
+    .withMessage("Last name is required"),
   handleValidationErrors,
 ];
 
@@ -58,6 +46,44 @@ const validateLogin = [
     .withMessage('Password is required'),
   handleValidationErrors
 ];
+
+const isUniqueName = async (req, res, next) => {
+  const { username } = req.body;
+  const user = await User.findAll({
+    where: {
+      username: username
+    }
+  })
+
+  // res.json(user);
+
+  if(user.length > 0){
+     const err = new Error('User already exists');
+     err.status = 403;
+     err.errors = [
+      "User with that username already exists"
+     ]
+     next(err);
+  }
+};
+
+isUniqueEmail = async (req, res, next) => {
+  const { email } = req.body;
+  const user = await User.findAll({
+    where: {
+      email: email
+    }
+  })
+
+  if(user.length > 0){
+     const err = new Error('User already exists');
+     err.status = 403;
+     err.errors = [
+      "User with that email already exists"
+     ]
+     next(err);
+  }
+}
 
 //login route
 router.post(
@@ -137,9 +163,11 @@ router.get('/currentuser', async (req, res, next) => {
 
 }),
 // Sign up
-router.post("", validateUniqueName, validateSignup, async (req, res) => {
+router.post("", validateSignup, async (req, res, next) => {
   const { email, password, username, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
+  await isUniqueName(req, res, next);
+  await isUniqueEmail(req, res, next);
   const user = await User.create({ email, username, firstName, lastName, hashedPassword });
 
   const safeUser = {
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,21 +1,7 @@
 const { validationResult } = require('express-validator');
 const { User } = require('../db/models')
 
-isUniqueName = (value, req, res, next) => {
-  const user = User.findOne({
-    where: {
-      'username': value
-    }
-  })
 
-  if(user){
-     const err = new Error('User already exists');
-     err.status = 403;
-     err.errors = [
-      "User with that username already exists"
-     ]
-  }
-}
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
@@ -36,23 +22,6 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
-const UserAlreadyExistsErrors = (req, _res, next) => {
-  const validationErrors = validationResult(req);
-
-  if (!validationErrors.isEmpty()) {
-    const errors = [];
-    validationErrors
-      .array()
-      .forEach(error => errors.push(error.msg));
-
-    const err = Error("User already exists");
-    err.errors = errors;
-    err.status = 403;
-    err.title = "Bad request.";
-    next(err);
-  }
-  next();
-};
 module.exports = {
-  handleValidationErrors, UserAlreadyExistsErrors
+  handleValidationErrors,
 };
