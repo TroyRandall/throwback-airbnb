a41654ab6ffb254bbf4976fc54cfc3cc1577a2e8
TroyRandall <troyrandall18@gmail.com>
2023-04-19 13:11:11 -0400

    updated options object in Reviews migration

--- backend/db/migrations/20230418001005-create-rev-image.js
+++ backend/db/migrations/20230418001005-create-rev-image.js
@@ -8,7 +8,7 @@ if( process.env.NODE_ENV === 'production') {
 module.exports = {
   async up(queryInterface, Sequelize) {
     options.tableName = 'RevImages';
-    await queryInterface.createTable('RevImages', {
+    await queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
