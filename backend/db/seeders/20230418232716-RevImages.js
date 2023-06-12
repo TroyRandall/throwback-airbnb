f982f68a3315e43d278331f43e5f2188316f2334
TroyRandall <troyrandall18@gmail.com>
2023-06-12 17:48:11 -0400

    updated seeders

--- backend/db/seeders/20230418232716-RevImages.js
+++ backend/db/seeders/20230418232716-RevImages.js
@@ -11,7 +11,7 @@ module.exports = {
     await queryInterface.bulkInsert(options, [
       {
         url: 'thisIsAnImage.jpeg',
-        reviewId: 1,
+        reviewId: 3,
         userId: 1
       },
       {
