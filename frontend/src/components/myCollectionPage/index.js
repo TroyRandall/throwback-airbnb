ce3a0f538ac9ddd7c55097e66826b0fafd3e885f
TroyRandall <troyrandall18@gmail.com>
2023-07-10 18:13:12 -0400

    updated MyCollection component, if the user has no spots in collection then a button for creating a spot will appear, otherwise the collection will be displayed

--- frontend/src/components/myCollectionPage/index.js
+++ frontend/src/components/myCollectionPage/index.js
@@ -1,45 +1,58 @@
 import React from "react";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import PreviewUserSpot from './previewUserSpot.js'
-import * as currentActions from '../../store/current.js';
-import './myCollection.css';
+import PreviewUserSpot from "./previewUserSpot.js";
+import * as currentActions from "../../store/current.js";
+import "./myCollection.css";
 import UpdateSpotButton from "../../components/updateSpotButton";
 import DeleteSpotButton from "../../components/deleteSpotButton";
+import CreateSpotButton from "../../components/createSpotButton";
 
-function MyCollection () {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const[isLoaded, setIsLoaded] = useState(false);
-    const sessionUser = useSelector((state) => state.session.user)
-    const spots = useSelector((state) => state.spots)
+function MyCollection() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user);
+  const spots = useSelector((state) => state.spots);
 
+  useEffect(() => {
+    dispatch(currentActions.spotsByUser()).then(() => setIsLoaded(true));
+  }, [dispatch, spots]);
 
+  if (!sessionUser && isLoaded) history.push("/");
 
-    useEffect(() => {
-        dispatch(currentActions.spotsByUser()).then(() => setIsLoaded(true));
-      }, [ dispatch, spots])
+  const userSpots = useSelector((state) => state.current.current);
 
-      if (!sessionUser && isLoaded) history.push('/')
-
-      const userSpots = useSelector((state) => state.current.current);
-
-
-      return isLoaded && (
-        <div className='user-spots-container'>
-            {userSpots.map(spot => {
-
-               return ( <div key={spot.id} id='collection-container'>
-                 <PreviewUserSpot  key={spot.id} spot={spot} />
-                <UpdateSpotButton key={spot.id+50} spotId={spot.id} />
-                <DeleteSpotButton key={spot.id+100} id={spot.id}/>
-                </div>)
-            })}
-
-            </div>
+  if (userSpots && Object.values(userSpots).length < 1) {
+    return (
+      isLoaded && (
+        <div>
+          <h1>Manage spots</h1>
+          <CreateSpotButton />
+        </div>
       )
-
+    );
+  } else {
+    return (
+      isLoaded && (
+        <>
+          <h1>Manage spots</h1>
+          <div className="user-spots-container">
+            {userSpots.map((spot) => {
+              return (
+                <div key={spot.id} id="collection-container">
+                  <PreviewUserSpot key={spot.id} spot={spot} />
+                  <UpdateSpotButton key={spot.id + 50} spotId={spot.id} />
+                  <DeleteSpotButton key={spot.id + 100} id={spot.id} />
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )
+    );
+  }
 }
 export default MyCollection;
